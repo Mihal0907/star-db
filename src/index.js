84083f06416445d6d6f94a904bc098a75acaeb87
Andrey <miandmih@gmail.com>
2019-02-17 22:02:28 +0200

    Added application template, added SWAPI integration via FetchAPI

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-console.log('Hello')
+ReactDOM.render(<App />,
+    document.getElementById('root'));
